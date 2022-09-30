385ef7f33a546eb574e0265141c753edfcd2e3ee
SaM <89473536+MorningStar1540@users.noreply.github.com>
2022-10-01 01:10:21 +0530

    Update percentage.py

--- percentage.py
+++ percentage.py
@@ -1,9 +1,12 @@
-n=int(input("Enter number of subjects: ))
-m=int(input("Enter maximum marks: ))
+n=int(input("Enter number of subjects: "))
+m=int(input("Enter maximum marks: "))
+if m==0:
+            print("Invalid INPUT")
+            m=int(input("Enter maximum marks: "))
 t=0
 m=m*n
 for i in range (n):
-            a=int(input("Enter marks of ",n+1," subject))
+            a=int(input("Enter marks of ",n+1," subject"))
             t=a+t
 per=(t/m)*100
 pritn("Percentage gain is: ",per)                       
